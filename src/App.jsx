c6e307eb71eee9bf70fdecf4514958da3e62ca12
OliverBarta <barta3738@gmail.com>
2026-06-04 14:36:59 -0400

    cleared a lot of the default react template stuff and started a top bar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import TopBar from './TopBar.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <TopBar/>
+     <div>hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/TopBar.jsx
@@ -0,0 +1,16 @@
+
+import './TopBar.css'
+
+
+function TopBar() {
+
+    return (
+        <>
+            <div className='topBar'>
+                <div className='title'>Book Site</div>
+            </div>
+        </>
+    )
+}
+
+export default TopBar
