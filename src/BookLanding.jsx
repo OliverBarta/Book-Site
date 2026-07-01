f66fccc30bc248258a60a1037556c288e6cd37b5
OliverBarta <barta3738@gmail.com>
2026-07-01 16:29:29 -0400

    added animation to the input box for the chapter

--- src/BookLanding.jsx
+++ src/BookLanding.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import "./BookLanding.css"
 import { useParams } from 'react-router-dom';
 import { supabase } from './supabaseClient';
@@ -14,10 +14,30 @@ function BookLanding() {
     const [description, setDescription] = useState("");
     const [imgAddress, setImgAddress] = useState("");
     const [continueChapter, setContinueChapter] = useState(1);
-    const [chapterInput, setChapterInput] = useState(1);
+    const [chapterInput, setChapterInput] = useState("");
     
 
     const { title, bookId } = useParams();
+
+    // this variable and the following useEffect are used to prevent the input box from transitioning when the window is resized, which can cause a jarring effect
+    const inputRef = useRef(null);
+    useEffect(() => {
+        let resizeTimeout;
+
+        function handleResize() {
+            inputRef.current?.classList.add('no-transition');
+            clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(() => {
+                inputRef.current?.classList.remove('no-transition');
+            }, 100);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            clearTimeout(resizeTimeout);
+        };
+    }, []);
 
     useEffect(() => {
         async function getBookDetails() {
@@ -43,7 +63,6 @@ function BookLanding() {
 
             if (savedChapter) {
                 setContinueChapter(parseInt(savedChapter, 10));
-                setChapterInput(parseInt(savedChapter, 10));
             } else {
                 setContinueChapter(1);
                 
@@ -74,11 +93,13 @@ function BookLanding() {
                 <p>{description}</p>
             </div>
             <div className='buttonRow'>
-                <form onSubmit={handleSubmit} className='inputChapterBox'>
-                    <input type="text" placeholder="Enter chapter" autoComplete="off" className='inputChapter'
+                <form onSubmit={handleSubmit} className={`inputChapterBox${chapterInput ? ' active' : ''}`}>
+                    <input type="text" placeholder="Enter chapter" autoComplete="off" className={`inputChapter${chapterInput ? ' active' : ''}`}
+                        value={chapterInput}
                         onChange={(e) => setChapterInput(e.target.value)}
+                        ref={inputRef}
                     ></input>
-                    <button className='buttonSearch'>Go</button>
+                    <button type='submit' className='buttonSearch'>Go</button>
                 </form>
                 
                 <button className='readFrom'
